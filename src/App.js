edbb96a6e065dbf4dbd9ee43f4b28424cb1666ad
mohsetiawanmodeong <mohsetiawanmodeong@gmail.com>
2022-05-21 23:44:47 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,71 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Parentbox">
+      <FotoProduk />
+      <ProdukInfo isDiscount="yes" title="Uyo Dora" category="SEPATU" />
     </div>
   );
+}
+
+function FotoProduk() {
+  return (
+    <div className="Foto">
+      <img src="sepatu.jpg" alt="sepatu" />
+    </div>
+  )
+}
+
+function CheckDiscount(props) {
+  const { isDiscount } = props;
+  if (isDiscount == "yes") {
+    return (
+      <p>Diskon 50% Off</p>
+    );
+  }
+  else if (isDiscount == "coming") {
+    return (
+      <p>Akan ada diskon...</p>
+    );
+  }
+  else {
+    return (
+      <p>Belum ada diskon</p>
+    );
+  }
+}
+
+function ProdukInfo(props) {
+  const { category, title, isDiscount } = props;
+  const benefits = ["Tahan lama", "Kuat lama", "Tahan banting", "Tahan air"];
+  const listBenefits = benefits.map((itemBenefit) =>
+    <li>{itemBenefit}</li>
+  );
+  const price = "74.000.000";
+  return (
+    <div>
+      <div className="Deskripsi">
+        <p className="Cate">{category}</p>
+        <h1 className="Title">{title}</h1>
+        <p className="Price">IDR {price}</p>
+        <CheckDiscount isDiscount={isDiscount} />
+        <p className="Info">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque, blanditiis
+          adipisci obcaecati tempora reprehenderit corporis repellat? Tempore quisquam
+          officiis, optio iure praesentium dolore omnis mollitia. Eos minus minima
+          praesentium laboriosam.
+        </p>
+        <ul>
+          {listBenefits}
+        </ul>
+        <a onClick={(e) => TambahCart(title, e)} href="#">Add to Cart</a>
+      </div>
+    </div>
+  );
+}
+
+function TambahCart(e) {
+  return console.log("Membeli " + e);
 }
 
 export default App;
